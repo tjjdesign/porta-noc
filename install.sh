f6f56e6d553cc48ca461a20e25a2d3e05663e0ee
Thomas James <tjjdesign@outlook.com>
2020-02-13 14:55:55 -0700

    Added gitignore, basic bash functions
    Logs will rotate with a new install.latest for the latest run, and all previous logs get added onto install.log.

--- install.sh
+++ install.sh
@@ -1,11 +1,45 @@
+#!/bin/bash
 # PORTA NOC PRE-PRODUCTION
-
+clear
 head -10 readme.md
 
-#Install required network tools/firewall
+#Log install actions
+if (test -f "install.latest"); then
+    cat install.latest >> install.log
+    rm install.latest
+    exec > >(tee -i install.latest)
+else
+    exec > >(tee -i install.latest)
+fi
+
+#date for log
+printf "\n"
+date
+
+#Check for sudo
+if [[ $EUID -ne 0 ]]; then
+    printf "This installer must be run as root.\n"
+    exit 1
+fi
+
+#Check/Install required network tools/firewall
+printf "# Verifiying network requirments are met. #\n"
+#Checking for internet/DNS connectivity
+printf "Checking for internet connectivity..."
+sleep 1
+if ping -q -c 1 -W 1 google.com >/dev/null; then
+  echo "SUCCESS"
+else
+  echo "FAIL"
+  printf "\nNo internet connectivity.\n"
+  exit 1
+fi
 
 #Install splunk
 
 #Install greylog
 
 #Install moloch/kismet/ettercap/bettercap
+
+echo "Complete."
+sleep 2
